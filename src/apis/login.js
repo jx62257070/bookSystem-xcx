417c98be44ae36300c43ff7c6c821a46c0f5a863
shengjie.chen <shengjie.chen@xiaobao100.com>
2019-01-31 11:55:41 +0800

    aa

--- src/apis/login.js
+++ src/apis/login.js
@@ -2,8 +2,13 @@ import request from '@/utils/request'
 
 const LoginServie = {}
 
+//登陆接口
 LoginServie.login=function(data){
     return request.post('/Login/login',data)
+}
+//注册接口
+LoginServie.register=function(data){
+    return request.post('/Login/register',data)
 }
 
 
--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -1,16 +1,53 @@
 <template>
-    <div>
-        注册页面
+  <div>
+      <div style="display: flex;justify-content:center;">
+          <div class="title">注册页面</div>
+        </div>
+    <div class="box">
+      <div class="boxInner">
+        <form action>
+          <div class="nei1">
+            <Input prefix="ios-contact" placeholder="请输入用户名" class="name" v-model="userName"/>
+          </div>
+          <div class="nei2">
+            <Input
+              type="password"
+              prefix="md-lock"
+              placeholder="请输入密码"
+              class="name"
+              v-model="userPass"
+            />
+          </div>
+          <div class="nei4">
+            <Button type="info" style="width:360px;font-size:18px" @click.native="register">register</Button>
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
 <script>
+import LoginServie from "@/apis/login"
+
 export default {
-    data(){
-        return {
-            
-        }
-    }
-}
+  data() {
+    return {
+      userName: "",
+      userPass: "",
+    };
+  },
+  methods: {
+      register:async function () {
+          let data={
+              userName:this.userName,
+              userPass:this.userPass,
+          }
+          let result =await LoginServie.register(data)
+          console.log(result);
+          
+      }
+  }
+};
 </script>
 <style scoped>
 @import "./register.css";
